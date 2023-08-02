a514c39671675b1033861a9a487f3001a269aa09
Giulio Procopio <giulioprocopioo@gmail.com>
2023-08-02 12:07:36 +0200

    Fix missing verbose functions stubs

--- include/qq/verbose.h
+++ include/qq/verbose.h
@@ -1,6 +1,8 @@
 #ifndef QQ_VERBOSE_H
 #define QQ_VERBOSE_H
 
+#include <stdbool.h>
+
 typedef enum {
     QQ_QUIET = 0,
     QQ_ERROR = 1,
@@ -22,5 +24,17 @@ QQVerbose qq_verbose = QQ_DEFAULT;
 #else
 extern QQVerbose qq_verbose;
 #endif
+
+QQVerbose
+qq_get_verbose(void);
+
+void
+qq_set_verbose(QQVerbose v);
+
+void
+qq_setenv_verbose(const char *env);
+
+bool
+qq_is_verbose(QQVerbose v);
 
 #endif
